27c5ed8cfe66a95b187bdb23efb7ceff29645f2b
YongKi Lee <koogi012@gmail.com>
2023-10-27 09:17:15 +0900

    first commit

---
+++ android/app/src/main/kotlin/kr/ne/skycom/flutter_practice_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package kr.ne.skycom.flutter_practice_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_practice_project/item_list_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shopping App',
+      home: const ItemListPage(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+      ),
+    );
+  }
+}
